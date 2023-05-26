7ec6901816572ac84debb0836d0571b0f8300b46
austin-showen <austin.showen@gmail.com>
2023-05-26 12:40:43 -0500

    add express-session

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const session = require('express-session')
+
+require('dotenv').config()
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -17,6 +20,15 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+)
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
